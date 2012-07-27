733d517c2809612c058c826f2409252048431ca2
U-Diablo-PC\Diablo <Diablo@Diablo-PC.(none)>
2012-07-27 16:25:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c10f1f95adf2af8a8999e2dda4d61bcd5ddabc93ef3e5664d7063b1485406378e17869a959141717aa32dfa7c2777290320be42d674d892ae26195577b7e0e9c'
